7e349b7765967cc090769604a35bd2209f7d0338
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-23 02:54:56 +0530

    Prototypes and bitwise helpers

--- sim.sh
+++ sim.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # ./build_champsim.sh bimodal no no no no lru 1
-# ./build_champsim.sh bimodal no dbcp no no lru 1
+./build_champsim.sh bimodal no dbcp no no lru 1
 
-binaries=(bimodal-no-no-no-no-lru-1core)
+binaries=(bimodal-no-dbcp-no-no-lru-1core)
 for bin in "${binaries[@]}"; do
 	for trace in dpc3_traces/*; do
-		time ./run_champsim.sh $bin 1 15 $(basename $trace)
+		time ./run_champsim.sh $bin 1 5 $(basename $trace)
 	done
 done
 
