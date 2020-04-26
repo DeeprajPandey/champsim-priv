af663d0f4d5a35f1f761a734fcd141a74ca9c13e
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-04-26 19:19:14 +0530

    Set up script to download traces, build models and run sims

--- sim.sh
+++ sim.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+rm -rf bin/
+
+cd script/
+./download_dpc3_traces.sh
+cd ../
+
 # ./build_champsim.sh bimodal no no no no lru 1
 # ./build_champsim.sh bimodal no next_line no no lru 1
+# Original Model
 ./build_champsim.sh bimodal no dbcp dbcp_sup no lru 1
+# Keep non-critical misses
+./build_champsim.sh bimodal no dbcp no no lru 1
 
-binaries=(bimodal-no-dbcp-dbcp_sup-no-lru-1core)
+# Our model
+./build_champsim.sh bimodal no dbcp_AXR dbcp_sup no lru 1
+# Keep non-critical misses
+./build_champsim.sh bimodal no dbcp_AXR no no lru 1
+
+# bimodal-no-dbcp_AXR-dbcp_sup-no-lru-1core bimodal-no-dbcp_AXR-no-no-lru-1core
+binaries=(bimodal-no-dbcp-dbcp_sup-no-lru-1core bimodal-no-dbcp-no-no-lru-1core)
 for bin in "${binaries[@]}"; do
 	for trace in dpc3_traces/*; do
 		time ./run_champsim.sh $bin 1 5 $(basename $trace)
